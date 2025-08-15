889f0987c371b9c4262e894cafaad0bccd83c88e
ilbeyataberkcan <ilbeyataberk.can@hotmail.com>
2025-08-15 22:56:24 +0300

    Moving forward with Lsp setup. Almost there... Just need to take an hour or so to finish the setup. Then I can start programming.

--- nvim/.config/nvim/lua/v0/plugins/lsp.lua
+++ nvim/.config/nvim/lua/v0/plugins/lsp.lua
@@ -1,7 +1,23 @@
+local function attach(opts)
+    vim.keymap.set('n', "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', "K", function () vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set('n', "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('n', "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end
+
+
 return {
     {
         "neovim/nvim-lspconfig",
         dependencies = {
+            { 'mason-org/mason.nvim', opts = {} },
+            'mason-org/mason-lspconfig.nvim',
             'saghen/blink.cmp',
             {
                 "folke/lazydev.nvim",
@@ -18,8 +34,18 @@ return {
             local capabilities = require('blink.cmp').get_lsp_capabilities()
             local lspconfig = require('lspconfig')
 
+            require('mason').setup()
+            require('mason-lspconfig').setup {
+                ensure_installed = {
+                    "lua_ls",
+                    "clangd"
+                }
+            }
+
             -- add language servers
-            lspconfig.lua_ls.setup {capabilities = capabilities}
+            lspconfig.lua_ls.setup { capabilities = capabilities }
+            lspconfig.clangd.setup { capabilities = capabilities }
+            attach()
         end
     }
-}
+} 
