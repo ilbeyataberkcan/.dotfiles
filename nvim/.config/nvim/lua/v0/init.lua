1335c2e8abcc90dbadd74bea0836079d9fc1a332
ilbeyataberkcan <ilbeyataberk.can@hotmail.com>
2025-08-16 01:00:03 +0300

    Well it should be working now... But even though it identifies the errors, It doesn't tell me what they are... I believe I didn't setup the blink completion correctly. So, it conflicts with other lsp functionality. Will check on it later.

--- nvim/.config/nvim/lua/v0/init.lua
+++ nvim/.config/nvim/lua/v0/init.lua
@@ -1,2 +1,26 @@
 require("v0.set")
 require("v0.remap")
+
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function (ev)
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        vim.keymap.set('n', "gd", function() vim.lsp.buf.definition() end)
+        vim.keymap.set('n', "gD", function() vim.lsp.buf.declaration() end)
+        vim.keymap.set('n', "K", function () vim.lsp.buf.hover() end)
+        vim.keymap.set('n', "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+        vim.keymap.set('n', "<leader>vd", function() vim.diagnostic.open_float() end)
+
+        -- Setup Trouble instead...
+        vim.keymap.set('n', "[d", function() vim.diagnostic.goto_next() end)
+        vim.keymap.set('n', "]d", function() vim.diagnostic.goto_prev() end)
+
+        vim.keymap.set('n', "<leader>vca", function() vim.lsp.buf.code_action() end)
+        vim.keymap.set('n', "<leader>vrr", function() vim.lsp.buf.references() end)
+        vim.keymap.set('n', "<leader>vrn", function() vim.lsp.buf.rename() end)
+        vim.keymap.set('n', "<C-h>", function() vim.lsp.buf.signature_help() end)
+    end
+
+})
--- nvim/.config/nvim/lua/v0/plugins/lsp.lua
+++ nvim/.config/nvim/lua/v0/plugins/lsp.lua
@@ -1,17 +1,3 @@
-local function attach(opts)
-    vim.keymap.set('n', "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', "K", function () vim.lsp.buf.hover() end, opts)
-    vim.keymap.set('n', "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set('n', "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set('n', "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set('n', "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n', "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set('n', "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set('n', "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set('n', "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end
-
-
 return {
     {
         "neovim/nvim-lspconfig",
@@ -39,13 +25,16 @@ return {
                 ensure_installed = {
                     "lua_ls",
                     "clangd"
+                },
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup {
+                            capabilities = capabilities
+                        }
+                    end
                 }
-            }
 
-            -- add language servers
-            lspconfig.lua_ls.setup { capabilities = capabilities }
-            lspconfig.clangd.setup { capabilities = capabilities }
-            attach()
+            }
         end
     }
 } 
